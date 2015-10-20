d1e9a3f562293e8ad3af388d0233042ef316afeb
Jonathon Douglas <jdouglas@acquisinsurance.com>
2015-10-20 09:28:35 +0100

    Tidy up SQL Server Backend

--- statsd.net/Backends/SqlServer/SqlServerBackend.cs
+++ statsd.net/Backends/SqlServer/SqlServerBackend.cs
@@ -1,29 +1,27 @@
 ﻿using System;
-using System.ComponentModel.Composition;
-using System.Data;
-using System.Data.SqlClient;
-using System.Threading.Tasks;
-using System.Threading.Tasks.Dataflow;
-using System.Xml.Linq;
-using Polly;
-using Microsoft.SqlServer.Server;
-using statsd.net.Configuration;
-using statsd.net.core;
-using statsd.net.core.Backends;
-using statsd.net.core.Messages;
-using statsd.net.core.Structures;
-using statsd.net.shared;
-using statsd.net.Logging;
 
 namespace statsd.net.Backends.SqlServer
 {
-  [Export(typeof(IBackend))]
+  using System.ComponentModel.Composition;
+  using System.Data;
+  using System.Data.SqlClient;
+  using System.Threading.Tasks;
+  using System.Threading.Tasks.Dataflow;
+  using System.Xml.Linq;
+  using Microsoft.SqlServer.Server;
+  using Polly;
+  using statsd.net.core;
+  using statsd.net.core.Backends;
+  using statsd.net.core.Messages;
+  using statsd.net.core.Structures;
+  using statsd.net.Configuration;
+  using statsd.net.Logging;
+
+  [Export(typeof (IBackend))]
   public class SqlServerBackend : IBackend
   {
     private string _connectionString;
     private string _collectorName;
-    private bool _isActive;
-    private Task _completionTask;
     private BatchBlock<GraphiteLine> _batchBlock;
     private ActionBlock<GraphiteLine[]> _actionBlock;
     private static SqlMetaData[] statsdTable = { new SqlMetaData("measure", SqlDbType.VarChar, 255) };
@@ -32,13 +30,14 @@ namespace statsd.net.Backends.SqlServer
     private Policy _retryPolicy;
     private readonly ILog _log = LogProvider.GetCurrentClassLogger();
 
-    public string Name { get { return "SqlServer"; } }  
+    public string Name => "SqlServer";
 
     public void Configure(string collectorName, XElement configElement, ISystemMetricsService systemMetrics)
     {
       _systemMetrics = systemMetrics;
 
-      var config = new SqlServerConfiguration(configElement.Attribute("connectionString").Value, configElement.ToInt("writeBatchSize"));
+      var config = new SqlServerConfiguration(configElement.Attribute("connectionString").Value,
+        configElement.ToInt("writeBatchSize"));
 
       _connectionString = config.ConnectionString;
       _collectorName = collectorName;
@@ -47,32 +46,27 @@ namespace statsd.net.Backends.SqlServer
       InitialiseRetryHandling();
 
       _batchBlock = new BatchBlock<GraphiteLine>(config.WriteBatchSize);
-      _actionBlock = new ActionBlock<GraphiteLine[]>(p => SendToDB(p), new ExecutionDataflowBlockOptions() { MaxDegreeOfParallelism = 1 });
+      _actionBlock = new ActionBlock<GraphiteLine[]>(p => SendToDB(p),
+        new ExecutionDataflowBlockOptions { MaxDegreeOfParallelism = 1 });
       _batchBlock.LinkTo(_actionBlock);
 
       _batchBlock.Completion.ContinueWith(p => _actionBlock.Complete());
-      _actionBlock.Completion.ContinueWith(p => { _isActive = false; });
+      _actionBlock.Completion.ContinueWith(p => { IsActive = false; });
 
-      _completionTask = new Task(() =>
+      Completion = new Task(() =>
       {
         _log.Info("SqlServerBackend - Completion has been signaled. Waiting for action block to complete.");
         _batchBlock.Complete();
         _actionBlock.Completion.Wait();
       });
-
     }
 
-    public bool IsActive
-    {
-      get { return _isActive; }
-    }
+    public bool IsActive { get; private set; }
 
-    public int OutputCount
-    {
-      get { return _batchBlock.OutputCount; }
-    }
+    public int OutputCount => _batchBlock.OutputCount;
 
-    public DataflowMessageStatus OfferMessage(DataflowMessageHeader messageHeader, Bucket messageValue, ISourceBlock<Bucket> source, bool consumeToAccept)
+    public DataflowMessageStatus OfferMessage(DataflowMessageHeader messageHeader, Bucket messageValue,
+      ISourceBlock<Bucket> source, bool consumeToAccept)
     {
       messageValue.FeedTarget(_batchBlock);
       return DataflowMessageStatus.Accepted;
@@ -80,13 +74,10 @@ namespace statsd.net.Backends.SqlServer
 
     public void Complete()
     {
-      _completionTask.Start();
+      Completion.Start();
     }
 
-    public Task Completion
-    {
-      get { return _completionTask; }
-    }
+    public Task Completion { get; private set; }
 
     public void Fault(Exception exception)
     {
@@ -95,10 +86,12 @@ namespace statsd.net.Backends.SqlServer
 
     private void InitialiseRetryHandling()
     {
-        _retryPolicy = Policy.Handle<SqlException>().WaitAndRetry(_retries, retryAttempt => TimeSpan.FromSeconds(1), (exception, timeSpan) =>
+      _retryPolicy = Policy.Handle<SqlException>()
+        .WaitAndRetry(_retries, retryAttempt => TimeSpan.FromSeconds(1), (exception, timeSpan) =>
         {
-            _log.WarnException(String.Format("Retry failed. Trying again. Delay {1}, Error: {2}", timeSpan, exception.Message), exception);
-            _systemMetrics.LogCount("backends.sqlserver.retry");
+          _log.WarnException(
+            string.Format("Retry failed. Trying again. Delay {1}, Error: {2}", timeSpan, exception.Message), exception);
+          _systemMetrics.LogCount("backends.sqlserver.retry");
         });
     }
 
@@ -111,7 +104,7 @@ namespace statsd.net.Backends.SqlServer
         foreach (var line in lines)
         {
           row = tableData.NewRow();
-          row["rowid"] = System.DBNull.Value;
+          row["rowid"] = DBNull.Value;
           row["source"] = this._collectorName;
           row["metric"] = line.ToString();
           tableData.Rows.Add(row);
@@ -119,15 +112,15 @@ namespace statsd.net.Backends.SqlServer
         _log.DebugFormat("Attempting to send {0} lines to tb_Metrics.", tableData.Rows.Count);
 
         _retryPolicy.Execute(() =>
+        {
+          using (var bulk = new SqlBulkCopy(_connectionString))
           {
-            using (var bulk = new SqlBulkCopy(_connectionString))
-            {
-              bulk.DestinationTableName = "tb_Metrics";
-              bulk.WriteToServer(tableData);
-            }
-            _systemMetrics.LogCount("backends.sqlserver.lines", tableData.Rows.Count);
-            _log.DebugFormat("Wrote {0} lines to tb_Metrics.", tableData.Rows.Count);
-          });
+            bulk.DestinationTableName = "tb_Metrics";
+            bulk.WriteToServer(tableData);
+          }
+          _systemMetrics.LogCount("backends.sqlserver.lines", tableData.Rows.Count);
+          _log.DebugFormat("Wrote {0} lines to tb_Metrics.", tableData.Rows.Count);
+        });
       }
       catch (Exception ex)
       {
@@ -139,9 +132,9 @@ namespace statsd.net.Backends.SqlServer
     public DataTable CreateEmptyTable()
     {
       DataTable outputTable = new DataTable();
-      outputTable.Columns.Add("rowid", typeof(int));
-      outputTable.Columns.Add("source", typeof(string));
-      outputTable.Columns.Add("metric", typeof(string));
+      outputTable.Columns.Add("rowid", typeof (int));
+      outputTable.Columns.Add("source", typeof (string));
+      outputTable.Columns.Add("metric", typeof (string));
       return outputTable;
     }
   }
